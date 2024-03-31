5dd1aa75d47f2cf9ab12ba9a932a4beaf50dd626
Thanh Nhan <npthanhnhan2003@gmail.com>
2024-03-31 21:37:25 +0700

    init project

---
+++ src/main/java/com/example/QuanLyKTX/QuanLyKyTucXaApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuanLyKTX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyKyTucXaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyKyTucXaApplication.class, args);
+	}
+
+}
